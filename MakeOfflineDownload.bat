2d88843246723bb0e23e2c706b521edc37583d07
Orion Leon <121742281+zumbiepig@users.noreply.github.com>
2024-05-06 10:29:33 -0700

    jh

--- MakeOfflineDownload.bat
+++ MakeOfflineDownload.bat
@@ -1,4 +1,5 @@
-@echo off
+@echo on
 title MakeOfflineDownload
-java -cp "desktopRuntime/MakeOfflineDownload.jar;desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.html" "javascript/classes.js" "javascript/assets.epk" "javascript/Shadow_Client_Offline_en_US.html" "javascript/Shadow_Client_Offline.html" "javascript/lang"
+java -cp "desktopRuntime/MakeOfflineDownload.jar;desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.html" "javascript/classes.js" "javascript/assets.epk" "nul" "javascript/Shadow_Client_Offline.html" "javascript/lang/"
+dir javascript/
 pause
